cdbddd817f8bbbadf9caf82a98733851f7dd800c
Tamal Saha <tamal@appscode.com>
2019-07-22 14:11:12 -0700

    Add ByteBuilders models
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ gitea/cluster.go
@@ -0,0 +1,84 @@
+package gitea
+
+import (
+	"bytes"
+	"encoding/json"
+	"time"
+
+	pharmerApi "pharmer.dev/pharmer/apis/v1beta1"
+)
+
+// Cluster represents a kubernetes cluster
+type Cluster struct {
+	ID          int64              `json:"id"`
+	Owner       *User              `json:"owner"`
+	Name        string             `json:"name"`
+	Private     bool               `json:"private"`
+	Parent      *Cluster           `json:"parent"`
+	Archived    bool               `json:"archived"`
+	Data        pharmerApi.Cluster `json:"data"`
+	OperationID string             `json:"operation_id,omitempty"`
+	// swagger:strfmt date-time
+	Created time.Time `json:"created_at"`
+	// swagger:strfmt date-time
+	Updated time.Time `json:"updated_at"`
+	// swagger:strfmt date-time
+	Deleted *time.Time `json:"deleted_at"`
+}
+
+// ClusterMetadata represents metadata of a cluster
+type ClusterMetadata struct {
+	Config                    *ClusterMetadataResponse_KubedConfig `json:"config,omitempty"`
+	CustomResourceDefinitions []string                             `json:"customResourceDefinitions,omitempty"`
+	Upgrades                  []*pharmerApi.Upgrade                `json:"upgrades,omitempty"`
+}
+
+type SSHConfigGetResponse struct {
+	Config *pharmerApi.SSHConfig `json:"config,omitempty"`
+}
+
+type ClusterMetadataResponse_KubedConfig struct {
+	//Version             *appscode_version.Version `json:"version,omitempty"`
+	SearchEnabled       bool `json:"searchEnabled,omitempty"`
+	ReverseIndexEnabled bool `json:"reverseIndexEnabled,omitempty"`
+	AnalyticsEnabled    bool `json:"analyticsEnabled,omitempty"`
+}
+
+type ClusterOperation struct {
+	OperationID string `json:"operation_id"`
+}
+
+// CreateClusterOption options when creating kubernetes cluster
+// swagger:model
+
+type CreateClusterOption struct {
+	// Name of the cluster to create
+	//
+	// required: true
+	// unique: true
+	Name string `json:"name" binding:"Required;AlphaDashDot;MaxSize(100)"`
+	// Configuration of the cluster to create
+	Configuration pharmerApi.Cluster `json:"configuration" binding:"Required"`
+	// Whether the cluster is private
+	Private bool `json:"private"`
+}
+
+// CreateCluster creates a cluster for authenticated user.
+func (c *Client) CreateCluster(opt CreateClusterOption) (*Cluster, error) {
+	body, err := json.Marshal(&opt)
+	if err != nil {
+		return nil, err
+	}
+	cluster := new(Cluster)
+	return cluster, c.getParsedResponse("POST", "/user/clusters", jsonHeader, bytes.NewReader(body), cluster)
+}
+
+type ImportClusterOption struct {
+	Provider                  string `json:"provider" binding:"Required"`
+	KubeConfig                string `json:"kube_config" binding:"Required"`
+	Region                    string `json:"region" binding:"Required"`
+	Zone                      string `json:"zone" binding:"Required"`
+	SSHKey                    string `json:"ssh_key"`
+	CredentialSecret          string `json:"credential_secret" binding:"Requried"`
+	CredentialSecretNamespace string `json:"credential_secret_namespace" binding:"Requried"`
+}
--- gitea/go.mod
+++ gitea/go.mod
@@ -2,4 +2,15 @@ module code.gitea.io/sdk/gitea
 
 go 1.12
 
-require code.gitea.io/gitea v1.10.0-dev.0.20190711052757-a0820e09fbf7
+require (
+	code.gitea.io/gitea v1.10.0-dev.0.20190711052757-a0820e09fbf7
+	k8s.io/api v0.0.0-20190531132109-d3f5f50bdd94
+	pharmer.dev/cloud v0.3.0
+	pharmer.dev/pharmer v0.4.0
+	sigs.k8s.io/cluster-api v0.1.7
+)
+
+replace (
+	github.com/renstrom/fuzzysearch => github.com/lithammer/fuzzysearch v1.0.1-0.20160331204855-2d205ac6ec17
+	k8s.io/api => k8s.io/api v0.0.0-20190313235455-40a48860b5ab
+)
